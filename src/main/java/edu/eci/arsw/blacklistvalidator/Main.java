54121788bdf13abfd6bb54a4c982f5db4d6e949f
unknown <juan.cadavid-p@labinfo.is.escuelaing.edu.co>
2022-01-19 09:52:31 -0500

    ARSW-LAB1:Primer 1.2.1

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -21,7 +21,16 @@ public class Main {
         List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55");
         System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
 
-        CountThread countThread = new CountThread(0, 99);
+        //Parte 1.1
+        //CountThread countThread = new CountThread(0, 99);
+
+        //Parte 1.2.1
+        //CountThread countThread1 = new CountThread(0, 99);
+        //CountThread countThread2 = new CountThread(99, 199);
+        //CountThread countThread3 = new CountThread(200, 299);
+
+        //Parte 1.2.2
+        
     }
     
 }
